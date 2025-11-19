1a031e5199a702be75446d2b94421a8d8c358e28
Julia <130534616+blurryvizion@users.noreply.github.com>
2025-11-19 15:02:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ $(function () {
   $('.play-button').click(clickedPlayButton);
   $('.exercise-button').click(clickedExerciseButton);
   $('.nap-button').click(clickedNapButton);
+
+  // set active style on whichever button is clicked
+  $('.button-container button').click(function () {
+    setActiveButton($(this));
+  });
 });
 
 // Pet object
@@ -20,9 +25,8 @@ var pet_info = {
 function clickedTreatButton() {
   pet_info.happiness += 1;
   pet_info.weight += 1;
-  pet_info.energy -= 1;
 
-  showPetMessage("Michi chirps happily after the seeds!");
+  showPetMessage("<strong>Michi</strong> chirps happily after the seeds!");
   checkAndUpdatePetInfoInHtml();
 }
 
@@ -32,7 +36,7 @@ function clickedPlayButton() {
   pet_info.weight -= 1;
   pet_info.energy -= 2;
 
-  showPetMessage("Michi shreds the cardboard box with joy!");
+  showPetMessage("<strong>Michi</strong> shreds the cardboard box with joy!");
   checkAndUpdatePetInfoInHtml();
 }
 
@@ -40,9 +44,9 @@ function clickedPlayButton() {
 function clickedExerciseButton() {
   pet_info.happiness -= 1;
   pet_info.weight -= 2;
-  pet_info.energy += 2;
+  pet_info.energy += 1;
 
-  showPetMessage("Michi flies laps around the house looking for food!");
+  showPetMessage("<strong>Michi</strong> flies laps around the house!");
   checkAndUpdatePetInfoInHtml();
 }
 
@@ -51,47 +55,23 @@ function clickedNapButton() {
   pet_info.energy += 3;
   pet_info.happiness += 1;
 
-  showPetMessage("Michi takes a cozy nap on your shoulder.");
+  showPetMessage("<strong>Michi</strong> takes a cozy nap on your shoulder.");
   checkAndUpdatePetInfoInHtml();
 }
 
-// Update and clamp stats
+// Clamp stats and update html
 function checkAndUpdatePetInfoInHtml() {
-  if (pet_info.weight < 0) 
-  {
-    pet_info.weight = 0;
-    showPetMessage("Michi is too skinny! Feed her more treats!");
-  }
-  if (pet_info.happiness < 0) 
-  {
-    pet_info.happiness = 0;
-    showPetMessage("Michi is sad! Play with her to cheer her up!");
-  }
-  if (pet_info.energy < 0) 
-  {
-    pet_info.energy = 0;
-    showPetMessage("Michi is exhausted! Let her take a nap!");
-  }
-  
-  if (pet_info.weight > 20)
-  {
-    pet_info.weight = 20;
-    showPetMessage("Michi is overweight! Exercise with her to slim down!");
-  }
+  if (pet_info.weight < 0) pet_info.weight = 0;
+  if (pet_info.happiness < 0) pet_info.happiness = 0;
+  if (pet_info.energy < 0) pet_info.energy = 0;
 
-  if (pet_info.happiness > 100)
-  { 
-    pet_info.happiness = 100;
-  }
-  if (pet_info.energy > 10) 
-  {
-    pet_info.energy = 10;
-  }
+  if (pet_info.happiness > 10) pet_info.happiness = 10;
+  if (pet_info.energy > 10) pet_info.energy = 10;
 
   updatePetInfoInHtml();
 }
 
-// Write stats to HTML
+// Write stats to page
 function updatePetInfoInHtml() {
   $('.name').text(pet_info.name);
   $('.weight').text(pet_info.weight);
@@ -99,16 +79,22 @@ function updatePetInfoInHtml() {
   $('.energy').text(pet_info.energy);
 }
 
-// Show message with jQuery effects
-function showPetMessage(message) {
-  var $msg = $('#pet-message');
-  $msg.text(message);
-
-  // fadeIn and fadeOut are the two new jQuery methods
-  $msg.fadeIn(200);
-  setTimeout(function () {
-    $msg.fadeOut(400);
-  }, 1000);
+// Highlight active button using siblings()
+function setActiveButton(button) {
+  // siblings(): selects the other buttons next to this one
+  button.addClass('active-btn');
+  button.siblings('button').removeClass('active-btn');
 }
 
+// Show pet message using html()
+function showPetMessage(message) {
+  var msg = $('#pet-message');
 
+  // html(): sets HTML content (allows <strong> tags)
+  msg.html(message);
+
+  msg.fadeIn(200);
+  setTimeout(function () {
+    msg.fadeOut(400);
+  }, 1000);
+}
